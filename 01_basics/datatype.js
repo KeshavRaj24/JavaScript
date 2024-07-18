5de1c6e36243eff8098f07d1beae5a3ee384187d
KeshavRaj24 <125239856+KeshavRaj24@users.noreply.github.com>
2024-07-18 12:39:19 +0000

    new files

---
+++ 01_basics/datatype.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all  code are newer version 
+
+
+//alert(3+3);  // we  are using  node js,not browser 
+
+let  name="Keshav";
+let age=25;
+let isLogined=true;
+let status=null;
+
+
+// Number  >2  to Power 53
+// String =" "
+// BigInt
+// boolean= true/false
+// null => Standlone Value
+// undefined => variable declear but not initiliezed.
+// symbol => unique 
+
+
+// object
+
+console.log(typeof undefined);
+console.log(typeof null);
+
---
+++ 01_basics/variable.js
@@ -0,0 +1,23 @@
+const accountId=1234234;
+let accountEmail="kumarkeshavsingh45@gmail.com";
+var accoutPassword="12345";
+accountCity="Noida"
+let accountStatus;
+
+
+//accountId=23; not allowed const value
+console.log(accountId);
+
+accountEmail="hd@gamil.com";
+accountCity="Delhi";
+  console.log(accountEmail);
+  console.log(accountCity);
+  console.table([accountId,accountEmail,accoutPassword,accountCity,accountStatus]);
+
+
+
+  /*
+
+  prefer not use to var 
+  because of issues in block scope and function scope.
+  */
